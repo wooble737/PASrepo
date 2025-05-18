595205e1de42ebc0a74d030f3877823f213dfd57
OlehBarboskin <iseeassyt@gmail.com>
2025-05-18 22:52:39 +0200

    5/18 commit changing main

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # PAS
 This is where funny mod.
 
-<img src="PAS.png" width="150" alt="the mod's logo" />
+<img src="logo.png" width="150" alt="the mod's logo" />
 
 # Resources
 * [Geode SDK Documentation](https://docs.geode-sdk.org/)
@@ -9,3 +9,7 @@ This is where funny mod.
 * [Geode CLI](https://github.com/geode-sdk/cli)
 * [Bindings](https://github.com/geode-sdk/bindings/)
 * [Dev Tools](https://github.com/geode-sdk/DevTools)
+
+# License
+
+MIT License
--- src/main.cpp
+++ src/main.cpp
@@ -1,49 +1,64 @@
+#include "Geode/binding/CCMenuItemSpriteExtra.hpp"
 #include "Geode/binding/FLAlertLayer.hpp"
 #include "Geode/binding/GameLevelManager.hpp"
 #include "Geode/binding/LevelInfoLayer.hpp"
 #include "Geode/binding/MenuGameLayer.hpp"
+#include "Geode/cocos/sprite_nodes/CCSprite.h"
 #include "Geode/loader/Log.hpp"
 #include "Geode/modify/Modify.hpp" 
 #include "Geode/ui/BasedButtonSprite.hpp"
 #include "Geode/ui/Popup.hpp"
+#include "ccTypes.h"
 #include <Geode/Geode.hpp>
 #include <Geode/modify/MenuLayer.hpp>
 #include <Geode/modify/LevelInfoLayer.hpp>
 #include <cmath>
+#include <cstddef>
+#include <windef.h>
+#include <winuser.h>
 
 using namespace geode::prelude;
 
 class $modify(MyLevelInfoLayer, LevelInfoLayer){
+
+	//struct Fields{
+	//	ccColor3B m_backgroundColor;
+	//};
 	$override
 	bool init(GJGameLevel *level, bool challenge){
 		if (!LevelInfoLayer::init(level, challenge)) return false;
 
-
-		auto buttonSprite = CircleButtonSprite::createWithSprite("PAS.png"_spr, 1.0f,CircleBaseColor::DarkPurple,CircleBaseSize::Medium);
-		buttonSprite->getTopNode()->setScale(1.0f);
+		auto buttonSprite = ButtonSprite::create("PAS");
 		auto myButton = CCMenuItemSpriteExtra::create(
-			CCSprite::createWithSpriteFrameName("GJ_likeBtn_001.png"),
+			buttonSprite,
 			this,
 			menu_selector(MyLevelInfoLayer::onMyButton)
 		);
 		myButton->setID("pas-button"_spr);
 
-		auto leftSideMenu = this->getChildByID("left-side-menu");
+		auto leftSideMenu = getChildByID("left-side-menu");
 		leftSideMenu->addChild(myButton);
 		leftSideMenu->updateLayout();
 
-
+		// auto levelIdVariable = GameLevelManager::get()->getSavedLevel(m_level);
+		// if (!levelIdVariable) return true;
+		// m_playBtnMenu-> getChildByID("play-menu")
+		// ->getChildByID("play-button")
+		// ->getChildByType<CCSprite>(0);
+		// return true;                        THIS IS THE GARBAGE THAT I WAS TRYING TO DO BY MYSELF TO JUST TRIGGER THE PLAYBUTTON
 		return true;
 	}
 
+
 	void onMyButton (CCObject*){
 		geode::createQuickPopup(
-			"PAS confirmation",
-			"Are you sure?",
+			"PAS",
+			"Start the Calculation?",
 			"Cancel",
 			"OK",
 			[](FLAlertLayer* layer, bool selected) -> void {
 				log::info("Selected {}", selected);
+				PlayLayer::get()->togglePracticeMode(true); //Just got this in, but this gets the thing inside of PlayLayer, while I am modifiying LevelInfoLayer
 			}
 		);
 	}
